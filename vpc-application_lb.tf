1cb4912dbc66ec6b817b8f948a68424dce74b4b0
Haris <harisgujjar317@gmail.com>
2023-02-17 20:37:23 -0500

    outputs

--- vpc-application_lb.tf
+++ vpc-application_lb.tf
@@ -17,4 +17,30 @@ module "application-load-balancer" {
             target_group_index = 0
         }
     ]
+    // target group
+    target_groups = [{
+        name_prefix      = "pref-"
+        backend_protocol = "HTTP"
+        backend_port     = 80
+        target_type      = "instance"
+        health_check = {
+            enabled             = true
+            interval            = 30
+            path                = "/app1/index.html"
+            port                = "traffic-port"
+            healthy_threshold   = 3
+            unhealthy_threshold = 3
+            timeout             = 6
+            protocol            = "HTTP"
+            matcher             = "200-399"
+        }
+        targets = {
+            my_target = {
+                target_id = module.ec2_instance_private.id[0]
+                port = 80 }
+            my_other_target = {
+                target_id = module.ec2_instance_private.id[1]
+                port = 8080 }
+        }
+    }]
 }
